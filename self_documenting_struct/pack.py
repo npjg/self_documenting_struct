9ef34d6a2a11f5edc102bae2e99d63368631a450
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2022-10-11 22:25:59 -0400

    Create packing into binary stream semantics

--- self_documenting_struct/pack.py
+++ self_documenting_struct/pack.py
@@ -9,33 +9,66 @@ from .data_types import Type
 ## All these methods are "atomic": They write only one item each.
 ## \param[in] value - The value to encode. This value must fit in the
 ##                    space provided in the target data type.
-## \return The value encoded in the target binary data type.
-def uint8(value: int) -> bytes:
-    return struct.pack(Type.uint8, value)
+## \param[in] into - If provided, the encoded value is written to this
+##                   binary stream rather than being returned.
+## \return The value encoded in the target binary data type,
+##         or None if the value was written into a stream.
+def uint8(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.uint8, value)
+    else:
+        struct.pack_into(Type.uint8, into, into.tell(), value)
 
-def int8(value: int) -> bytes:
-    return struct.pack(Type.int8, value)
+def int8(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.int8, value)
+    else:
+        struct.pack_into(Type.int8, into, into.tell(), value)
 
-def uint16_le(value: int) -> bytes:
-    return struct.pack(Type.uint16_le, value)
+def uint16_le(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.uint16_le, value)
+    else:
+        struct.pack_into(Type.uint16_le, into, into.tell(), value)
 
-def int16_le(value: int) -> bytes:
-    return struct.pack(Type.int16_le, value)
+def int16_le(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.int16_le, value)
+    else:
+        struct.pack_into(Type.int16_le, into, into.tell(), value)
 
-def uint16_be(value: int) -> bytes:
-    return struct.pack(Type.uint16_be, value)
+def uint16_be(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.uint16_be, value)
+    else:
+        struct.pack_into(Type.uint16_be, into, into.tell(), value)
 
-def int16_be(value: int) -> bytes:
-    return struct.pack(Type.int16_be, value)
+def int16_be(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.int16_be, value)
+    else:
+        struct.pack_into(Type.int16_be, into, into.tell(), value)
     
-def uint32_le(value: int) -> bytes:
-    return struct.pack(Type.uint32_le, value)
+def uint32_le(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.uint32_le, value)
+    else:
+        struct.pack_into(Type.uint32_le, into, into.tell(), value)
 
-def int32_le(value: int) -> bytes:
-    return struct.pack(Type.int32_le, value)
+def int32_le(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.int32_le, value)
+    else:
+        struct.pack_into(Type.int32_le, into, into.tell(), value)
 
-def uint32_be(value: int) -> bytes:
-    return struct.pack(Type.uint32_be, value)
+def uint32_be(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.uint32_be, value)
+    else:
+        struct.pack_into(Type.uint32_be, into, into.tell(), value)
 
-def int32_be(value: int) -> bytes:
-    return struct.pack(Type.int32_be, value)
+def int32_be(value: int, into = None) -> bytes:
+    if into is None:
+        return struct.pack(Type.int32_be, value)
+    else:
+        struct.pack_into(Type.int32_be, into, into.tell(), value)
